4318f24d5480a0e1fe7a834157d72f21a00ade57
GeorgeHanna <ghanna610@gmail.com>
2020-12-12 13:59:37 +0200

    Refactored code

--- hackerrank.py
+++ hackerrank.py
@@ -16,30 +16,13 @@ def solveMeFirst(a,b):
 def simpleArraySum(ar):
     # Challenge name: Simple Array Sum
     # Challenge problem statment: https://www.hackerrank.com/challenges/simple-array-sum/problem
-    sum = 0
-    for i in ar:
-        sum += i
-    return sum
+    return sum(ar)
 
 def aVeryBigSum(ar):
     # Challenge name: A Very Big Sum
     # Challenge problem statment: https://www.hackerrank.com/challenges/a-very-big-sum/problem
-    if (1 <= len(ar) <= 10):
-        sum = 0
-        applicable = True
-        
-        for a in ar:
-            if (0 <= a <= 10**10):
-                sum += a
-            else:
-                applicable = False
-        
-        if applicable:
-            return sum
-        else:
-            return
-    else:
-        return
+    return sum(ar)    
+
 
 def diagonalDifference(arr):
     # Challenge name: Diagonal Difference
@@ -62,78 +45,52 @@ def diagonalDifference(arr):
 def plusMinus(arr):
     # Challenge name: Plus Minus
     # Challenge problem statment: https://www.hackerrank.com/challenges/plus-minus/problem
-    if (0 < len(arr) <= 100):
-        pos, neg, zer = 0, 0, 0
-        for i in arr:
-            if (-100 <= i <= 100):
-                if (i > 0):
-                    pos += 1
-                elif (i < 0):
-                    neg += 1
-                else:
-                    zer += 1
-            else:
-                break
-        size = len(arr)
-        print("{:.6f}".format(pos / size))
-        print("{:.6f}".format(neg / size))
-        print("{:.6f}".format(zer / size))
-    else:
-        return
+    pos, neg, zer = 0, 0, 0
+    for i in arr:
+        if (i > 0):
+            pos += 1
+        elif (i < 0):
+            neg += 1
+        else:
+            zer += 1
+
+    size = len(arr)
+    print("{:.6f}".format(pos / size))
+    print("{:.6f}".format(neg / size))
+    print("{:.6f}".format(zer / size))
 
 def staircase(n):
     # Challenge name: Staircase
     # Challenge problem statment: https://www.hackerrank.com/challenges/staircase/problem
-    if (0 < n <= 100):
-        for i in range(n):
-            print((' ' * (n - i - 1)) + '#' * (i + 1))
-    else:
-        return
+    for i in range(n):
+        print((' ' * (n - i - 1)) + '#' * (i + 1))
 
 def miniMaxSum(arr):
     # Challenge name: Mini-Max Sum
     # Challenge problem statment: https://www.hackerrank.com/challenges/mini-max-sum/problem
     arr.sort()
     min_sum, max_sum = 0, 0
-    applicable = True
 
     for i ,a in enumerate(arr):
-        if (1 <= a <= 10**9):
-            if (i < len(arr) - 1):
-                min_sum += a
-            if (i != 0):
-                max_sum += arr[len(arr) - i]
-        else:
-            applicable = False
+        if (i < len(arr) - 1):
+            min_sum += a
+        if (i != 0):
+            max_sum += arr[len(arr) - i]
 
-    if applicable:
-        print("{} {}".format(min_sum, max_sum))
-    else:
-        return
+    print("{} {}".format(min_sum, max_sum))
 
 def birthdayCakeCandles(candles):
     # Challenge name: Birthday Cake Candles
     # Challenge problem statment: https://www.hackerrank.com/challenges/birthday-cake-candles/problem
     candles.sort()
-    if (1 <= len(candles) <= 10**5):
-        tallest = candles[len(candles) - 1]
-        count = 0
-        applicable = True
-        
-        for candle in reversed(candles):
-            if (1 <= candle <= 10**7): 
-                if (candle == tallest):
-                    count += 1
-                else:
-                    applicable = False
-                    break
+    tallest = candles[len(candles) - 1]
+    count = 0
 
-        if applicable:
-            return count
-        else:
-            return
-    else:
-        return
+    for candle in reversed(candles):
+        if (candle == tallest):
+            count += 1
+
+    return count
 
 def timeConversion(s):
     # Challenge name: Time Conversion
@@ -165,167 +122,128 @@ def compareTriplets(a, b):
     # Challenge problem statment: https://www.hackerrank.com/challenges/compare-the-triplets/problem
     result = [0, 0]
     for i in range(3):
-        if ((1 <= a[i] <= 100) and (1 <= b[i] <= 100)):
-            if a[i] > b[i]:
-                result[0] = result[0] + 1
-            elif (a[i] < b[i]):
-                result[1] = result[1] + 1
-            else:
-                continue
-        else:
-            break
+        if a[i] > b[i]:
+            result[0] = result[0] + 1
+        if (a[i] < b[i]):
+            result[1] = result[1] + 1
         
     return result 
 
 def gradingStudents(grades):
     # Challenge name: Grading Students
     # Challenge problem statment: https://www.hackerrank.com/challenges/grading/problem
-    if (1 <= len(grades) <= 100):
-        applicable = True
+    for i ,grade in enumerate(grades):
+        next_multiple = math.ceil(grade / 5) * 5
+        if (grade >= 38):
+            if (next_multiple - grade < 3):
+                grades[i] = next_multiple
 
-        for i ,grade in enumerate(grades):
-            next_multiple = math.ceil(grade / 5) * 5
-            
-            if (0 <= grade <= 100):
-                if (grade >= 38):
-                    if (next_multiple - grade < 3):
-                        grades[i] = next_multiple
-            else:
-                applicable = False
-
-        return (grades if applicable else None)
-    else:
-        return
+    return grades
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     # Challenge name: Apple and Orange
     # Challenge problem statment: https://www.hackerrank.com/challenges/apple-and-orange/problem
-    if ((1 <= s <= 10**5) and (1 <= t <= 10**5) and (1 <= a <= 10**5) 
-            and (1 <= b <= 10**5) and (1 <= len(apples) <= 10**5) 
-            and (1 <= len(oranges) <= 10**5) and (-10**5 <= s-t <= 10**5)
-            and (a < s < t < b)):
-        
-        fa, fo = 0, 0
+    fa, fo = 0, 0
 
-        for apple in apples:
-            if (s <= apple + a <= t):
-                fa += 1
+    for apple in apples:
+        if (s <= apple + a <= t):
+            fa += 1
         
-        for orange in oranges:
-            if (s <= orange + b <= t):
-                fo += 1
+    for orange in oranges:
+        if (s <= orange + b <= t):
+            fo += 1
         
-        print(fa)
-        print(fo)
-    else:
-        return
+    print(fa)
+    print(fo)
 
 def kangaroo(x1, v1, x2, v2):
     # Challenge name: Number Line Jumps
     # Challenge problem statment: https://www.hackerrank.com/challenges/kangaroo/problem
-    if ((0 <= x1 < x2 <= 10000) and (1 <= v1 <= 10000) and (1 <= v2 <= 10000)):
-        if (v1 > v2 and ((x2 - x1) % (v1 - v2) == 0)):
-            return "YES"
-        return "NO"
-    else:
-        return
+    if (v1 > v2 and ((x2 - x1) % (v1 - v2) == 0)):
+        return "YES"
+    return "NO"
 
 def pangrams(s):
     # Challenge name: Pangrams
     # Challenge problem statment: https://www.hackerrank.com/challenges/pangrams/problem
-    if ( 0 < len(s) <= 10**3):
-        s = s.lower().split()
-        s = ''.join(s)
-        ascii_set = set([ord(l) for l in s])
-
-        if (len(ascii_set) == 26):
-            return "pangram"
-        else:
-            return "not pangram"
+    s = s.lower().split()
+    s = ''.join(s)
+    ascii_set = set([ord(l) for l in s])
+        
+    if (len(ascii_set) == 26):
+        return "pangram"
     else:
-        return
+        return "not pangram"
 
 def minimumNumber(n, password):
     # Challenge name: Strong Password
     # Challenge problem statment: https://www.hackerrank.com/challenges/strong-password/problem
-    if (1 <= n <= 100):
-        count = 0
+    count = 0
 
-        if (not re.search('[a-z]', password)):
-            count += 1
-        if (not re.search('[A-Z]', password)):
-            count += 1
-        if (not re.search('[0-9]', password)):
-            count += 1
-        if (not re.search('[!@#$%^&*()-+]', password)):
-            count += 1
+    if (not re.search('[a-z]', password)):
+        count += 1
+    if (not re.search('[A-Z]', password)):
+        count += 1
+    if (not re.search('[0-9]', password)):
+        count += 1
+    if (not re.search('[!@#$%^&*()+-]', password)):
+        count += 1
 
-        return max(count, 6 - n)
-        
-    else:
-        return
+    return max(count, 6 - n)
 
 def breakingRecords(scores):
     # Challenge name: Breaking the Records
     # Challenge problem statment: https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
-    elements_constraint = all(0 <= score <= 10**8 for score in scores) 
-    if ((1 <= len(scores) <= 1000) and elements_constraint):
-        changes = [0, 0]
-        max_score, min_score = scores[0], scores[0]
-        
-        for i in range(1, len(scores)):
-            if (scores[i] < min_score):
-                min_score = scores[i]
-                changes[1] += 1
-            elif ((scores[i] > max_score)):
-                max_score = scores[i]
-                changes[0] += 1
-            else:
-                continue
 
-        return changes
+    changes = [0, 0]
+    max_score, min_score = scores[0], scores[0]
+        
+    for i in range(1, len(scores)):
+        if (scores[i] < min_score):
+            min_score = scores[i]
+            changes[1] += 1
+        if ((scores[i] > max_score)):
+            max_score = scores[i]
+            changes[0] += 1
+
+    return changes
 
 def birthday(s, d, m):
     # Challenge name: Sub-array Division
     # Challenge problem statment: https://www.hackerrank.com/challenges/the-birthday-bar/problem
-    elements_constraint = all(1 <= bar <= 5 for bar in s) 
-    if ((1 <= len(s) <= 100) and (1 <= d <= 31) and (1 <= m <= 12) and (elements_constraint)):
-        ways = 0
+    ways = 0
 
-        for i in range(len(s)):
-            sum_of_bars = s[i]
-            for j in range(i + 1, i + m):
-                if (j < len(s)):
-                    sum_of_bars += s[j]
+    for i in range(len(s)):
+        sum_of_bars = s[i]
+        for j in range(i + 1, i + m):
+            if (j < len(s)):
+                sum_of_bars += s[j]
             
-            if (sum_of_bars == d):
-                ways += 1
+        if (sum_of_bars == d):
+            ways += 1
 
-        return ways
+    return ways
 
 def divisibleSumPairs(n, k, ar):
     # Challenge name: Divisible Sum Pairs
     # Challenge problem statment: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
-    elements_constraint = all(1 <= a <= 100 for a in ar) 
-    if ((2 <= n <= 100) and (1 <= k <= 100) and (elements_constraint)):
-        pairs = 0
+    pairs = 0
 
-        for i in range(len(ar)):
-            for j in range(i + 1, len(ar)):
-                if (ar[i] + ar[j]) % k == 0:
-                    pairs += 1
+    for i in range(len(ar)):
+        for j in range(i + 1, len(ar)):
+            if (ar[i] + ar[j]) % k == 0:
+                pairs += 1
         
-        return pairs
+    return pairs
 
 def migratoryBirds(arr):
     # Challenge name: Migratory Birds
     # Challenge problem statment: https://www.hackerrank.com/challenges/migratory-birds/problem
     freq = [0, 0, 0, 0, 0] # Denoting [Type 1, Type 2, Type 3, Type 4, Type 5]
-    if (5 <= len(arr) <= 2*10**5):
-        for bird in arr:
-            freq[bird - 1] += 1
+    for bird in arr:
+        freq[bird - 1] += 1
         
-        return freq.index(max(freq)) + 1
+    return freq.index(max(freq)) + 1
 
 def dayOfProgrammer(year):
     # Challenge name: Day of the Programmer
@@ -358,41 +276,35 @@ def dayOfProgrammer(year):
 def bonAppetit(bill, k, b):
     # Challenge name: Bill Division
     # Challenge problem statment: https://www.hackerrank.com/challenges/bon-appetit/problem
-    bill_constraint = all(0 <= b <= 10**4 for b in bill)
-    bill_sum = sum(bill)
-    
-    if (2 <= len(bill) <= 10**5) and (0 <= k < len(bill)) and bill_constraint and (0 <= b <= bill_sum):
-        s = 0
-        for i in range(len(bill)):
-            if (i != k):
-                s += bill[i]
+    s = 0
+    for i in range(len(bill)):
+        if (i != k):
+            s += bill[i]
         
-        b_actual = s // 2
+    b_actual = s // 2
 
-        if b_actual == b:
-            print("Bon Appetit")
-        else:
-            print(b - b_actual)
+    if b_actual == b:
+        print("Bon Appetit")
+    else:
+        print(b - b_actual)
 
 def pageCount(n, p):
     # Challenge name: Drawing Book
     # Challenge problem statment: https://www.hackerrank.com/challenges/drawing-book/problem
-    if (1 <= n <= 10**5) and (1 <= p <= n):
+    if (p == n // 2):
+        return n // 4
 
-        if (p == n // 2):
-            return n // 4
-
-        if p == n:
-            return 0
+    if p == n:
+        return 0
         
-        if p == n - 1:
-            return 1
+    if p == n - 1:
+        return 1
         
-        if (p > n // 2):
-            return n // 2 - p // 2
+    if (p > n // 2):
+        return n // 2 - p // 2
 
-        if (p < n // 2):
-            return p // 2 
+    if (p < n // 2):
+        return p // 2 
 
 def countingValleys(steps, path):
     # Challenge name: Counting Valleys
@@ -465,6 +377,11 @@ def angryProfessor(k, a):
         if (a[i] <= 0):
             stud += 1
     return "NO" if stud >= k else "YES"
+
+def queensAttack(n, k, r_q, c_q, obstacles):
+    # Challenge name: Queen's Attack II
+    # Challenge problem statment: https://www.hackerrank.com/challenges/queens-attack-2/problem
+    
 
 if __name__ == "__main__":
     pass
