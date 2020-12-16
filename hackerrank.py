1a0e96fe9649b50cd9b3d9c7e414fbf8df5b1fe7
GeorgeHanna <ghanna610@gmail.com>
2020-12-16 12:39:14 +0200

    Solved Forming a Magic Square

--- hackerrank.py
+++ hackerrank.py
@@ -415,9 +415,35 @@ def queensAttack(n, k, r_q, c_q, obstacles=[[]]):
 
     return top + bottom + right + left + top_left + top_right + bottom_left + bottom_right
 
+def formingMagicSquare(s):
+    # Challenge name: Forming a Magic Square
+    # Challenge problem statment: https://www.hackerrank.com/challenges/magic-square-forming/problem 
+    magic_combination = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    ]
+    
+    if s in magic_combination:
+        return 0
+
+    cost = [0] * 8
+    for i, ele in enumerate(magic_combination):
+        for j in range(3):
+            for k in range(3):
+                cost[i] += abs(s[j][k] - ele[j][k])
+        
+    return min(cost)
+
 if __name__ == "__main__":
-    print(queensAttack(5, 3, 4, 3, [[5, 5], [4,  2], [2, 3]]))
-    print(queensAttack(8, 0, 4, 4))
-    print(queensAttack(4, 0, 4, 4))
-    print(queensAttack(4, 0, 3, 2))
-    print(queensAttack(8, 1, 4, 4, [[3, 5]]))
+    formingMagicSquare([[5, 3, 4], [1, 5, 8], [6, 4, 2]])
+    formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 5]])
+    formingMagicSquare([[4, 8, 2], [4, 5, 7], [6, 1, 6]])
+    formingMagicSquare([[4, 9, 2], [3, 5, 7], [8, 1, 6]])
+    formingMagicSquare([[9, 9, 8], [8, 9, 9], [8, 9, 9]])
+    formingMagicSquare([[2, 7, 6], [9, 5, 1], [4, 3, 8]])
